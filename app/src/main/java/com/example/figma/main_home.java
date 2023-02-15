1ad14b2058a4a4da2b6f07bbd7762d4a0af3e846
KSM <rlatpals100@naver.com>
2023-02-15 13:54:45 +0900

    main_home 바꿈 2월 8일 업로드

--- app/src/main/java/com/example/figma/main_home.java
+++ app/src/main/java/com/example/figma/main_home.java
@@ -9,18 +9,35 @@ import android.widget.Button;
 
 
 public class main_home extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_home);
 
         Button show_notice_more = findViewById(R.id.show_notice_more);
+        Button show_notice_writing = findViewById(R.id.show_notice_writing);
+        Button btn_timetable = findViewById(R.id.btn_timetable);
         show_notice_more.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), notice_list.class);
+                startActivity(intent);
+            }
+        });
+
+        show_notice_writing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), notice_writing.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_timetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Timetable.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/figma/notice_writing.java
+++ app/src/main/java/com/example/figma/notice_writing.java
@@ -1,6 +1,15 @@
 package com.example.figma;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class notice_writing extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice_writing);
+    }
 }
