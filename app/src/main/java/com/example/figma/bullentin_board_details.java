a30f0f5aaebaa3df203cc55cbfa15ff3b95d94f7
KSM <rlatpals100@naver.com>
2023-02-18 14:16:11 +0900

    main_home 고침

--- app/src/main/java/com/example/figma/bullentin_board_details.java
+++ app/src/main/java/com/example/figma/bullentin_board_details.java
@@ -1,6 +1,30 @@
 package com.example.figma;
 
-import android.app.Activity;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class bullentin_board_details extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bullentin_board_details extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bullentin_board_details);
+        ImageButton btn_amend = findViewById(R.id.btn_amend);
+
+
+        btn_amend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), bullentin_board_writing.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
 }
+
