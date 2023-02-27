7d45323aa69c799862898e1a0782c26fe1d5f51f
hanyong kim <hanyong8254@gmail.com>
2023-02-27 15:46:31 +0900

    김한용

--- app/src/main/java/com/example/figma/my_inf_details.java
+++ app/src/main/java/com/example/figma/my_inf_details.java
@@ -1,17 +1,27 @@
 package com.example.figma;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
 
 
 public class my_inf_details extends Activity {
+    private static final int REQUEST_CODE=0;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.my_inf_details);
 
         // 뒤로가기 버튼
         ImageButton backButton = findViewById(R.id.backButton);
@@ -23,5 +33,49 @@ public class my_inf_details extends Activity {
                 startActivity(intent);
             }
         });
+        // warningButton
+        Button warningButton = findViewById(R.id.warningButton);
+        warningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), warning_message.class);
+                startActivity(intent);
+            }
+        });
+
+
+        imageView=findViewById(R.id.changeImage);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == REQUEST_CODE)
+        {
+            if(requestCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "사진선택취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/figma/warning_message.java
+++ app/src/main/java/com/example/figma/warning_message.java
@@ -1,6 +1,14 @@
 package com.example.figma;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class warning_message extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.warning_message);
+    }
 }
